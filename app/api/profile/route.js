aca6a20564b2d27d2998b93d63089c96c0bb049d
IAGO PENIDO <89784278+iagopenido@users.noreply.github.com>
2025-10-22 00:45:18 -0300

    Fix import path for config.json

--- app/api/profile/route.js
+++ app/api/profile/route.js
@@ -1,4 +1,4 @@
-import cfg from '../../content/config.json'
+import cfg from '../../../content/config.json'
 export async function GET() {
   return new Response(JSON.stringify(cfg), { headers: { 'content-type': 'application/json' } });
 }
